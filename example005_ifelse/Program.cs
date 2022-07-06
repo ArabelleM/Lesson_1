f5ded88c7b23c1b7014272d1799a2af67137f539
Arabelle <yulitoniy@gmail.com>
2022-07-06 09:34:27 +0300

    programm for Hello Masha

---
+++ example005_ifelse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter user name: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "маша")
+{
+    Console.WriteLine("Это же Маша");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}cs
