bc80de915f4ccc711dea7d4900dfa2d0cf01f8d5
Arabelle <yulitoniy@gmail.com>
2022-07-06 09:13:52 +0300

    use random

--- hello_user/Program.cs
+++ hello_user/Program.cs
@@ -1,4 +1,10 @@
-﻿Console.WriteLine("Enter your name");
-string user_name = Console.ReadLine();
-Console.Write("Hello, ");
-Console.Write(user_name);
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Enter your name");
+        string user_name = Console.ReadLine();
+        Console.Write("Hello, ");
+        Console.Write(user_name);
+    }
+}
